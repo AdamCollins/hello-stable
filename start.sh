56e6a47bfe5c78a0fc083a3bc29223a020b32fa0
Adam Collins <adamcollins@Adams-MacBook-Air.local>
2020-01-25 17:30:11 -0800

    fixed start script

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 service nginx start
 
 npm start 3000 node1 &
+echo "node 1 started"
 npm start 3001 node2 &
+echo "node 2 started"
 npm start 3002 node3 &
-npm start 3003 node4 &
+echo "node 3 started"
+npm start 3003 node4
+echo "node 4 started"
