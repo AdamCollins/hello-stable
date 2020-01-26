8892e46872938efbedacca3d05dada642702fda7
Adam Collins <adamcollins@Adams-MacBook-Air.local>
2020-01-25 16:21:10 -0800

    Added nginx config

---
+++ node-server/main.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const hbs =  require('hbs');
+
+if(!process.argv[2]){
+	console.log('Please provide a port number. npm start <port>');
+	return;
+}
+const port = process.argv[2];
+let nodeName = process.argv[3] || 'default';
+
+app.set('view-engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+	res.render('index.hbs', { nodeName: nodeName, layout:'layout.hbs' });
+});
+
+app.listen(port, () => {
+	console.log(`${nodeName} app listening on port ${port}!`)
+});
